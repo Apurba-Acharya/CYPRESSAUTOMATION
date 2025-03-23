8631269faef7f8116270c3d3694d95519667f5ca
Apurba <apurba1907@gmail.com>
2025-03-23 13:47:27 +0530

    23/03

--- cypress/e2e/WebAutomation/CSSLocator.cy.js
+++ cypress/e2e/WebAutomation/CSSLocator.cy.js
@@ -2,91 +2,73 @@
 
 describe("CSSLocators", () =>{
 
-    it("CSS 1", () =>{
+    it("CSS 1 : ID: htmltag#id || #id", () =>{
         cy.visit ("http://www.automationpractice.pl/index.php");
-
-        //ID: htmltag#id || #id
         cy.get ("input#search_query_top").type("Trouser")
     })
 
-    it("CSS 2", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //CLASS: htmltag.classname || htmltag.c1.c2.c3...cn || .classname || .c1.c2.c3...cn
+    it("CSS 2: CLASS: htmltag.classname || htmltag.c1.c2.c3...cn || .classname || .c1.c2.c3...cn", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get ("input.search_query").type("T-Shirts"); // input.search_query.form-control.ac_input
     })
 
-    it("CSS 3", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //css selector using multiple one tagname and multiple id and value :
+    it("CSS 3: css selector using multiple one tagname and multiple id and value", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("input.search_query.form-control.ac_input#search_query_top").type("Shirt")
     })
 
-    it("CSS 4", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //htmltag[id='value']
+    it("CSS 4: htmltag[id='value']", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("a[class='login']").click();
     })
 
-    it("CSS 5", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //css locator using multiple attribute :
+    it("CSS 5: css locator using multiple attribute", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("input[id='search_query_top'][name='search_query']").type("VSCode")
     })
 
-    it("CSS 6", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //cantains in css locators :
+    it("CSS 6: cantains in css locators", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("input[class*='search']").type("DellTech")
     })
 
-    it("CSS 7", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //starting the text in css locators :
+    it("CSS 7: starting the text in css locators", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("input[class^='sear']").type("HpTechnology")
     })
 
-    it("CSS 8", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //ending the text in css locators :
+    it("CSS 8: ending the text in css locators", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("input[id$='top']").type("LenovoSlim")
     })
 
-    it("CSS 9", () =>{
-        cy.visit ("http://www.automationpractice.pl/index.php");
 
-        //comma the text in css locators :
+    it("CSS 9: comma the text in css locators", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
         cy.get("input.search_query,input#search_query_top").type("LenovoSlim")
     })
 
 
-    it("CSS 10", () =>{
+    it("CSS 10: nth-of-type in css locators", () =>{
         cy.visit ("http://www.automationpractice.pl/index.php");
-
-        //nth-of-type in css locators :
         cy.get("div#htmlcontent_home>ul>li:nth-of-type(3)").click();
     })
 
-    it("CSS 10", () =>{
+    it("CSS 11: Sibling in css locators", () =>{
         cy.visit ("http://www.automationpractice.pl/index.php");
-
-        //nth-of-type in css locators :
-        cy.get("").click();
-
-        //3. parenttag > childtag
-
-        //cy.get ("[name='search_query']").type("T-Shirts") //css selector - attribute
-
-        cy.get ("input.search_query[name='search_query']").type("T-Shirts") //css selector - tag class attribute
-
-        cy.get("button[name='submit_search']").click()
-
-        cy.get (".lighter").contains("T-Shirts")    //Assertion
+        cy.get("div.block_content>ul>li:nth-of-type(1)+li+li").click();
     })
+
+    it("CSS 12: contains() in css locators", () =>{
+        cy.visit ("http://www.automationpractice.pl/index.php");
+        cy.get("a[title*='Wom']").click();
+    })
+
 })
